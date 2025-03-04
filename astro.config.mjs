8757def108c665071e15b8eaf5aed844e95f4704
Joshua-Burle <joshua.burle@laplateforme.io>
2025-03-04 11:04:21 +0100

    Déploiement portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Joshuaburle.github.io',
+  base : 'CvJoshuaBurle', 
+  trailingSlash: 'always',
   integrations: [tailwind()],
 });
